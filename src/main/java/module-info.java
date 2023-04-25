253205e03fa96cb98d5da67e92fe2441ee9808a1
KatiaSahan <94547008+KatiaSahan@users.noreply.github.com>
2023-04-25 13:40:33 +0200

    Code  działanie okna logowanie i rejestracji

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module studia.projektzespolowy.kalendarz {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
-    requires java.sql;
-    requires mysql.connector.j;
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
 
