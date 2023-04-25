116d831bb6d5f4993e4d8a403295cda45a9a6591
KatiaSahan <94547008+KatiaSahan@users.noreply.github.com>
2023-04-25 14:11:51 +0200

    Revert "Code  działanie okna logowanie i rejestracji"
    This reverts commit 253205e03fa96cb98d5da67e92fe2441ee9808a1.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module studia.projektzespolowy.kalendarz {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.sql;
+    requires mysql.connector.j;
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
 
