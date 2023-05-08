e54210703a6d5d1f656f5822e57646f3173a453a
JaTuTylkoNaChwile <pawelbkr@gmail.com>
2023-05-08 16:07:53 +0200

    Trwają prace nad wywołaniem okna "Add Window", w którym będzie możliość dodawania zakładek do kalendarza. Na razie żaden inny plik .fxml oprócz "Calendar.fxml" nie chce się otworzyć i mnie szlag jasny zaraz trafi.

--- src/main/java/studia/projektzespolowy/kalendarz/CalendarController.java
+++ src/main/java/studia/projektzespolowy/kalendarz/CalendarController.java
@@ -2,20 +2,149 @@ package studia.projektzespolowy.kalendarz;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.TextField;
 
-public class CalendarController {
+import java.io.IOException;
+import java.net.URL;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.ResourceBundle;
+
+public class CalendarController implements Initializable {
 
     @FXML
-    private Button addButton;
+    public void addButtonClicked(ActionEvent ignoredEvent) throws IOException {
+        try {
+            System.out.println("User is adding an event ...");
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("AddWindow.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Add Window");
+            stage.setScene(new Scene(root));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("Error loading AddWindow.fxml: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
 
     @FXML
-    void Pressed(ActionEvent event) {
+    private ColorPicker ColorPicker;
 
+    @FXML
+    private Button ConfirmButton;
+
+    @FXML
+    private TextField EventNameLabel;
+
+
+    ZonedDateTime dateFocus;
+    ZonedDateTime today;
+
+    @FXML
+    private TextField year;
+
+    @FXML
+    private TextField month;
+
+    @FXML
+    public FlowPane calendar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        dateFocus = ZonedDateTime.now();
+        today = ZonedDateTime.now();
+        drawCalendar();
     }
 
-    public void addButtonClicked()
-    {
-
+    @FXML
+    void backOneMonth(ActionEvent ignoredEvent) {
+        dateFocus = dateFocus.minusMonths(1);
+        calendar.getChildren().clear();
+        drawCalendar();
     }
+
+    @FXML
+    void forwardOneMonth(ActionEvent ignoredEvent) {
+        dateFocus = dateFocus.plusMonths(1);
+        calendar.getChildren().clear();
+        drawCalendar();
+    }
+
+
+    private void drawCalendar() {
+        year.setText(String.valueOf(dateFocus.getYear()));
+        month.setText(String.valueOf(dateFocus.getMonth()));
+
+        double calendarWidth = calendar.getPrefWidth();
+        double calendarHeight = calendar.getPrefHeight();
+        double strokeWidth = 1;
+        double spacingH = calendar.getHgap();
+        double spacingV = calendar.getVgap();
+
+        int monthMaxDate = dateFocus.getMonth().maxLength();
+
+        //check for leap year
+        if(dateFocus.getYear() % 4 !=0 && monthMaxDate == 29){
+            monthMaxDate = 28;
+        }
+        int dateOffSet = ZonedDateTime.of(dateFocus.getYear(), dateFocus.getMonthValue(), 1,0,0,0,0,dateFocus.getZone()).getDayOfWeek().getValue();
+
+        for(int i=0; i<6; i++){
+            for(int j=0; j<7; j++){
+
+                StackPane stackPane = new StackPane();
+
+                Rectangle rectangle = new Rectangle();
+                rectangle.setFill(Color.TRANSPARENT);
+                rectangle.setStroke(Color.GRAY);
+                rectangle.setStrokeWidth(strokeWidth);
+                double rectangleWidth = (calendarWidth/7) - strokeWidth - spacingH;
+                rectangle.setWidth(rectangleWidth);
+                double rectangleHeight = (calendarHeight/6) - strokeWidth - spacingV;
+                rectangle.setHeight(rectangleHeight);
+                stackPane.getChildren().add(rectangle);
+
+                int calculateDate = (j + 1) + (7 * i);
+
+                if(calculateDate > dateOffSet){
+
+                    int currentDate = calculateDate - dateOffSet;
+
+                    if(currentDate <= monthMaxDate){
+                        Text date = new Text(String.valueOf(currentDate));
+                        double textTranslationY = - (rectangleHeight / 2) * 0.75;
+                        date.setTranslateY(textTranslationY);
+                        stackPane.getChildren().add(date);
+                    }
+                    if(today.getYear() == dateFocus.getYear() && today.getMonth() == dateFocus.getMonth() && today.getDayOfMonth() == currentDate){
+                        rectangle.setStroke(Color.BLUE);
+                    }
+                }
+                calendar.getChildren().add(stackPane);
+            }
+        }
+    }
+
+
 }
