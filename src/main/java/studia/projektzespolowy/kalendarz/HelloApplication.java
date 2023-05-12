03ab970869314dea3d88e563bc535e4e79c61118
Mateusz Liszewski <mateusz.liszewski2000@gmail.com>
2023-05-12 13:24:36 +0200

    Merge wszystkiego co mamy (poza najnowszymi zmianami Katji)

--- src/main/java/studia/projektzespolowy/kalendarz/HelloApplication.java
+++ src/main/java/studia/projektzespolowy/kalendarz/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 340, 500);
         stage.setTitle("Calendar");
         stage.setResizable(false);
         stage.setScene(scene);
